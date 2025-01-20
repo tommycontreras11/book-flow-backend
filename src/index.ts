e581b9f25229c8021a989c1b0596286493a8361e
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-19 20:04:39 -0400

    feat: :sparkles: add initial migration and uncomment function to connect to the database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import { connectDatabase } from "./database/connection"
 import app from "./app"
 
 async function main() {
-    //await connectDatabase()
-    app.listen(process.env.PORT || 3000)
-    console.log(`Server running on port ${process.env.PORT || 3000} 🚀`)
+    await connectDatabase()
+    app.listen(process.env.APP_PORT || 3000)
+    console.log(`Server running on port ${process.env.APP_PORT || 3000} 🚀`)
 }
 
 main()
