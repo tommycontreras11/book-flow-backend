5f423e05450cc60ac647168c33a2988fceedc16c
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-21 16:36:18 -0400

    fix: :bug: fix controller name

--- src/routers/api/country/index.ts
+++ src/routers/api/country/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllLanguageController } from "../../../controllers/language/getAll.controller";
+import { getAllCountryController } from "../../../controllers/country/getAll.controller";
 import { validateDTO } from "../../../middlewares/dto/validate-dto.middleware";
-import { getOneLanguageController } from "../../../controllers/language/getOne.controller";
+import { getOneCountryController } from "../../../controllers/country/getOne.controller";
 import { UuidDTO } from "../../../dto/common.dto";
 
 const router = Router()
 
-router.get('/', getAllLanguageController)
-router.get('/:uuid', validateDTO(UuidDTO, 'params'), getOneLanguageController)
+router.get('/', getAllCountryController)
+router.get('/:uuid', validateDTO(UuidDTO, 'params'), getOneCountryController)
 
 export default router
--- src/routers/api/language/index.ts
+++ src/routers/api/language/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllCountryController } from "../../../controllers/country/getAll.controller";
+import { getAllLanguageController } from "../../../controllers/language/getAll.controller";
 import { validateDTO } from "../../../middlewares/dto/validate-dto.middleware";
-import { getOneCountryController } from "../../../controllers/country/getOne.controller";
+import { getOneLanguageController } from "../../../controllers/language/getOne.controller";
 import { UuidDTO } from "../../../dto/common.dto";
 
-const router = Router()
+const router = Router();
 
-router.get('/', getAllCountryController)
-router.get('/:uuid', validateDTO(UuidDTO, 'params'), getOneCountryController)
+router.get("/", getAllLanguageController);
+router.get("/:uuid", validateDTO(UuidDTO, "params"), getOneLanguageController);
 
-export default router
+export default router;
