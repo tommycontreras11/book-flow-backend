8b6244cd4d9bd15823b46598a7d7f0cd85e83cfb
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-21 16:28:47 -0400

    feat: :sparkles: add services to get languages

---
+++ src/services/language/getAll.service.ts
@@ -0,0 +1,18 @@
+import { LanguageEntity } from "../../database/entities/entity/language.entity";
+import { FindManyOptions } from "typeorm";
+import { statusCode } from "../../utils/statusCode";
+
+export async function getAllLanguageService(options: FindManyOptions<LanguageEntity>) {
+  const languages = await LanguageEntity.find(options).catch((e) => {
+    console.error("LanguageEntity.find: ", e);
+    return null;
+  });
+
+  if (!languages)
+    return Promise.reject({
+      message: "Languages not found",
+      status: statusCode.NOT_FOUND,
+    });
+
+  return languages;
+}
---
+++ src/services/language/getOne.service.ts
@@ -0,0 +1,18 @@
+import { LanguageEntity } from "../../database/entities/entity/language.entity";
+import { FindOneOptions } from "typeorm";
+import { statusCode } from "../../utils/statusCode";
+
+export async function getOneLanguageService(options: FindOneOptions<LanguageEntity>) {
+  const language = await LanguageEntity.findOne(options).catch((e) => {
+    console.error("LanguageEntity.findOne: ", e);
+    return null;
+  });
+
+  if (!language)
+    return Promise.reject({
+      message: "Language not found",
+      status: statusCode.NOT_FOUND,
+    });
+
+  return language;
+}
