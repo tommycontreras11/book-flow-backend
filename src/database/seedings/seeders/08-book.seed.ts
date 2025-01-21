7c44ddb5f1d450400a44164bf05c9f63569371b9
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-21 18:11:42 -0400

    feat: :sparkles: add book-author data

--- src/database/seedings/seeders/08-book.seed.ts
+++ src/database/seedings/seeders/08-book.seed.ts
@@ -2,19 +2,32 @@ import { BookEntity } from "../../entities/entity/book.entity";
 import { DataSource } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
 import { BookData } from "../data/book.data";
+import { AuthorEntity } from "./../../../database/entities/entity/author.entity";
 
 export class BookSeeder implements Seeder {
     async run(_factory: Factory, dataSource: DataSource): Promise<void> {
         try {
             const bookRepository = dataSource.getRepository(BookEntity);
+            const authorRepository = dataSource.getRepository(AuthorEntity);
 
-            await Promise.all(BookData.map(async (book) => {
+            const authors = await authorRepository.find();
+
+            for (let i = 0; i < BookData.length; i++) {
+                const book = BookData[i];
+
                 const exists = await bookRepository.findOneBy({ description: book.description });
 
-                if(exists) return;
+                if (exists) continue;
 
-                await bookRepository.save(book);
-            }));
+                const bookCreated = await bookRepository.save(book);
+
+                const author = authors[i % authors.length]; 
+                await dataSource
+                    .createQueryBuilder()
+                    .relation(BookEntity, "authors") 
+                    .of(bookCreated) 
+                    .add(author);
+            }
         } catch (error) {
             console.error('BookSeeder -> run: ', error)
         }
