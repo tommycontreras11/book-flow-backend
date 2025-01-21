3d419462d77686e2ecd02f18cc02251a81474836
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-21 17:33:04 -0400

    fix: :bug: fix bibliography type data source

--- src/database/seedings/seeders/06-bibliography-type.seed.ts
+++ src/database/seedings/seeders/06-bibliography-type.seed.ts
@@ -1,14 +1,14 @@
-import { BibliographyTypeEntity } from "./../../../database/entities/entity/bibliography-type.entity";
 import { DataSource } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
-import { PublisherData } from "../data/publisher.data";
+import { BibliographyTypeData } from "../data/bibliography-type.data";
+import { BibliographyTypeEntity } from "./../../../database/entities/entity/bibliography-type.entity";
 
 export class BibliographyTypeSeeder implements Seeder {
     async run(_factory: Factory, dataSource: DataSource): Promise<void> {
         try {
             const bibliographyTypeRepository = dataSource.getRepository(BibliographyTypeEntity);
 
-            await Promise.all(PublisherData.map(async (bibliographyType) => {
+            await Promise.all(BibliographyTypeData.map(async (bibliographyType) => {
                 const exists = await bibliographyTypeRepository.findOneBy({ description: bibliographyType.description });
 
                 if(exists) return;
