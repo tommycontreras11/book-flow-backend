bc3d172ebe72484669e83d9da5cb66f71a1a0fef
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-02-02 13:58:13 -0400

    feat: :sparkles: add functionality to return book borrowed

--- src/dto/loan-management.dto.ts
+++ src/dto/loan-management.dto.ts
@@ -3,41 +3,14 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
-  MaxLength,
-  Validate,
-  ValidationArguments,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
+  MaxLength
 } from "class-validator";
 import { LoanManagementEnum } from "./../database/entities/entity/loan-management.entity";
-
-@ValidatorConstraint({ name: "DateLoanReturnValidation", async: false })
-class DateLoanReturnValidation implements ValidatorConstraintInterface {
-  validate(_value: any, args: ValidationArguments): boolean {
-    const { date_loan, date_return } = args.object as UpdateLoanManagementDTO;
-
-    const isDateLoanEmpty = !date_loan || date_loan.trim() === "";
-    const isDateReturnEmpty = !date_return || date_return.trim() === "";
-
-    return (
-      (isDateLoanEmpty && isDateReturnEmpty) ||
-      (!isDateLoanEmpty && !isDateReturnEmpty)
-    );
-  }
-
-  defaultMessage(_args: ValidationArguments): string {
-    return "Both date_loan and date_return must either be empty or both provided.";
-  }
-}
 
 export class CreateLoanManagementDTO {
   @IsString()
   @IsNotEmpty()
   date_loan: string;
-
-  @IsString()
-  @IsNotEmpty()
-  date_return: string;
 
   @IsString()
   @MaxLength(250)
@@ -48,12 +21,10 @@ export class CreateLoanManagementDTO {
 export class UpdateLoanManagementDTO {
   @IsString()
   @IsOptional()
-  @Validate(DateLoanReturnValidation)
   date_loan: string;
 
   @IsString()
   @IsOptional()
-  @Validate(DateLoanReturnValidation)
   date_return: string;
 
   @IsString()
--- src/services/loan-management/create.service.ts
+++ src/services/loan-management/create.service.ts
@@ -1,17 +1,23 @@
 import { generateLoanNumber } from "./../../utils/loan-management.util";
-import { RequestEntity, StatusRequestEnum } from "../../database/entities/entity/request.entity";
+import {
+  RequestEntity,
+  StatusRequestEnum,
+} from "../../database/entities/entity/request.entity";
 import { CreateLoanManagementDTO } from "../../dto/loan-management.dto";
 import { statusCode } from "../../utils/status.util";
-import { LoanManagementEntity, LoanManagementEnum } from "./../../database/entities/entity/loan-management.entity";
-import { getDaysBetweenDates } from "./../../utils/date.util";
+import {
+  LoanManagementEntity,
+  LoanManagementEnum,
+} from "./../../database/entities/entity/loan-management.entity";
+import { getDaysBetweenDates, getTodayFullDate } from "./../../utils/date.util";
 
 export async function createLoanManagementService(
   requestUUID: string,
-  { date_loan, date_return, comment }: CreateLoanManagementDTO
+  { date_loan, comment }: CreateLoanManagementDTO
 ) {
   const foundRequest = await RequestEntity.findOne({
     where: { uuid: requestUUID },
-    relations: { employees: true },
+    relations: { employees: true, loanManagements: true },
   }).catch((e) => {
     console.error("RequestEntity.findOne: ", e);
     return null;
@@ -29,18 +35,32 @@ export async function createLoanManagementService(
       status: statusCode.BAD_REQUEST,
     });
 
-    foundRequest.status = StatusRequestEnum.BORROWED;
-    await foundRequest.save();
+  const todayFullDate = getTodayFullDate();
+
+  const loanManagement = foundRequest.loanManagements;
+
+  const hasLoanManagementBorrow = loanManagement.some(
+    (loanManagement) => loanManagement.status === "BORROWED"
+  );
 
   await LoanManagementEntity.create({
-    loan_number: generateLoanNumber(),
-    date_loan,
-    date_return,
+    loan_number: hasLoanManagementBorrow
+      ? loanManagement[0].loan_number
+      : generateLoanNumber(),
+    date_loan: hasLoanManagementBorrow
+      ? loanManagement[0].date_loan
+      : date_loan,
+    ...(hasLoanManagementBorrow && { date_return: todayFullDate }),
     ...(comment && { comment }),
     amount_day: 20,
-    quantity_day: getDaysBetweenDates(date_loan, date_return),
+    quantity_day: getDaysBetweenDates(
+      hasLoanManagementBorrow ? loanManagement[0].date_loan : date_loan,
+      todayFullDate
+    ),
     request_id: foundRequest.id,
-    status: LoanManagementEnum.BORROWED,
+    status: hasLoanManagementBorrow
+      ? LoanManagementEnum.RETURNED
+      : LoanManagementEnum.BORROWED,
   })
     .save()
     .catch((e) => {
@@ -48,5 +68,19 @@ export async function createLoanManagementService(
       return null;
     });
 
-  return "Loan management created successfully";
+  await RequestEntity.update(
+    { id: foundRequest.id },
+    {
+      status: !hasLoanManagementBorrow
+        ? StatusRequestEnum.BORROWED
+        : StatusRequestEnum.COMPLETED,
+    }
+  ).catch((e) => {
+    console.error("RequestEntity.update: ", e);
+    return null;
+  });
+
+  return `Loan management ${
+    !hasLoanManagementBorrow ? "borrow" : "return"
+  } successfully`;
 }
