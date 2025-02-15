2c09d558b283fa90e0290136cb5b1cf425c23677
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-02-15 12:12:36 -0400

    feat: :sparkles: add minio configuration to connect to it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { connectDatabase } from "./database/connection"
 import app from "./app"
+import { ObjectStorage } from "./libs/object-storage"
 
 async function main() {
     await connectDatabase()
+    await ObjectStorage.instance.setConfiguration()
     app.listen(process.env.APP_PORT || 3000)
     console.log(`Server running on port ${process.env.APP_PORT || 3000} 🚀`)
 }
