37270611990e1d1a55cbf5df3bd587b147aebe61
TommyGC11 <tommycontreras34@gmail.com>
2025-03-21 13:04:44 -0400

    feat: :sparkles: simplify getAllRequestService by removing userLogged parameter and adjusting filters

--- src/services/request/getAll.service.ts
+++ src/services/request/getAll.service.ts
@@ -5,11 +5,8 @@ import { UserEntity } from "./../../database/entities/entity/user.entity";
 import { EmployeeEntity } from "./../../database/entities/entity/employee.entity";
 
 export async function getAllRequestService(
-  userLogged: UserEntity | EmployeeEntity | undefined,
   options: FindManyOptions<RequestEntity>
 ) {
-  if (!userLogged) return [];
-
   const requests = await RequestEntity.find(options).catch((e) => {
     console.error("RequestEntity.find: ", e);
     return null;
