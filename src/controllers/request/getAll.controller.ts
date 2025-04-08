41a5c3089cce7473c3b1cefb9bb3c1634f16c4d6
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-08 14:03:54 -0400

    refactor: improve getAllRequestController structure; enhance readability and maintainability

--- src/controllers/request/getAll.controller.ts
+++ src/controllers/request/getAll.controller.ts
@@ -5,34 +5,53 @@ import { StatusRequestEnum } from "./../../database/entities/entity/request.enti
 import { retrieveIfUserExists } from "./../../utils/user.util";
 import { UserEntity } from "./../../database/entities/entity/user.entity";
 import { In } from "typeorm";
+import { ObjectStorage } from "./../../libs/object-storage";
 
 export const getAllRequestController = async (req: Request, res: Response) => {
   const { status } = req.query as { status: StatusRequestEnum };
-  const foundUser = (await retrieveIfUserExists(UserEntity, null, null, req?.user?.uuid));
+  const foundUser = await retrieveIfUserExists(
+    UserEntity,
+    null,
+    null,
+    req?.user?.uuid
+  );
 
-  const statusArray = status != undefined ? [status].join(",").split(",") as StatusRequestEnum[] : [];
+  const statusArray =
+    status != undefined
+      ? ([status].join(",").split(",") as StatusRequestEnum[])
+      : [];
 
-const filters = {
-  ...(foundUser instanceof UserEntity && {
-    user: { id: foundUser.id },
-  }),
-  ...(statusArray.length > 0 && { status: In(statusArray) }),
-};
+  const filters = {
+    ...(foundUser instanceof UserEntity && {
+      user: { id: foundUser.id },
+    }),
+    ...(statusArray.length > 0 && { status: In(statusArray) }),
+  };
 
-  getAllRequestService({ where: filters, relations: { user: true, book: true } })
-    .then((data) => {
-      const requests = data.map((request) => ({
-        uuid: request.uuid,
-        user: {
-          uuid: request.user.uuid,
-          name: request.user.name,
-        },
-        book: {
-          uuid: request.book.uuid,
-          name: request.book.name,
-        },
-        status: request.status,
-      }));
+  getAllRequestService({
+    where: filters,
+    relations: { user: true, book: true },
+  })
+    .then(async (data) => {
+      const storage = ObjectStorage.instance;
+
+      const requests = await Promise.all(
+        data.map(async (request) => {
+          return {
+            uuid: request.uuid,
+            user: {
+              uuid: request.user.uuid,
+              name: request.user.name,
+            },
+            book: {
+              uuid: request.book.uuid,
+              name: request.book.name,
+              url: await storage.getUrl(request.book.file_name),
+            },
+            status: request.status,
+          };
+        })
+      );
 
       return res.status(statusCode.OK).json({ data: requests });
     })
