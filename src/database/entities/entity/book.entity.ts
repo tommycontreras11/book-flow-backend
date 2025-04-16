7aa7b350adb48fbcee53faa02259c285d3b232e5
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-16 13:30:56 -0400

    feat: :sparkles: add description and pages properties to BookEntity; enhance database schema for better book details

--- src/database/entities/entity/book.entity.ts
+++ src/database/entities/entity/book.entity.ts
@@ -1,74 +1,106 @@
-import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany } from "typeorm"
-import { BaseEntity, StatusEnum, StatusType } from "../base/base.entity"
-import { AuthorEntity } from "./author.entity"
-import { BibliographyTypeEntity } from "./bibliography-type.entity"
-import { LanguageEntity } from "./language.entity"
-import { PublisherEntity } from "./publisher.entity"
-import { RequestEntity } from "./request.entity"
-import { ScienceEntity } from "./science.entity"
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import { BaseEntity, StatusEnum, StatusType } from "../base/base.entity";
+import { AuthorEntity } from "./author.entity";
+import { BibliographyTypeEntity } from "./bibliography-type.entity";
+import { LanguageEntity } from "./language.entity";
+import { PublisherEntity } from "./publisher.entity";
+import { RequestEntity } from "./request.entity";
+import { ScienceEntity } from "./science.entity";
+import { GenreEntity } from "./genre.entity";
 
-@Entity({ name: 'books' })
+@Entity({ name: "books" })
 export class BookEntity extends BaseEntity {
-    @Column({ length: 250 })
-    name: string
+  @Column({ length: 250 })
+  name: string;
+  
+  @Column({ length: 500 })
+  description: string;
 
-    @Column()
-    topographical_signature: string
+  @Column()
+  topographical_signature: string;
 
-    @Column()
-    isbn: string
+  @Column()
+  isbn: string;
 
-    @Column()
-    bibliography_type_id: number
+  @Column()
+  pages: number;
 
-    @Column()
-    publisher_id: number
+  @Column()
+  bibliography_type_id: number;
 
-    @Column()
-    publication_year: number
+  @Column()
+  publisher_id: number;
 
-    @Column()
-    language_id: number
+  @Column()
+  publication_year: number;
 
-    @Column()
-    science_id: number
+  @Column()
+  language_id: number;
 
-    @Column()
-    file_name: string
-    
-    @Column({ type: 'enum', enum: StatusEnum })
-    status: StatusType
+  @Column()
+  science_id: number;
 
-    @ManyToMany(() => AuthorEntity, (author) => author.books)
-    @JoinTable({
-      name: 'book-authors',
-      joinColumn: {
-        name: 'bookId',
-        referencedColumnName: 'id'
-      },
-      inverseJoinColumn: {
-        name: 'authorId',
-        referencedColumnName: 'id'
-      }
-    })
-    authors: AuthorEntity[]
+  @Column()
+  file_name: string;
 
-    @ManyToOne(() => BibliographyTypeEntity, (bibliographyType) => bibliographyType.books)
-    @JoinColumn({ name: 'bibliography_type_id', referencedColumnName: 'id' })
-    bibliographyType: BibliographyTypeEntity
+  @Column({ type: "enum", enum: StatusEnum })
+  status: StatusType;
 
-    @ManyToOne(() => PublisherEntity, (publisher) => publisher.books)
-    @JoinColumn({ name: 'publisher_id', referencedColumnName: 'id' })
-    publisher: PublisherEntity
+  @ManyToMany(() => AuthorEntity, (author) => author.books)
+  @JoinTable({
+    name: "book-authors",
+    joinColumn: {
+      name: "bookId",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "authorId",
+      referencedColumnName: "id",
+    },
+  })
+  authors: AuthorEntity[];
 
-    @ManyToOne(() => LanguageEntity, (language) => language.books)
-    @JoinColumn({ name: 'language_id', referencedColumnName: 'id' })
-    language: LanguageEntity
+  @ManyToOne(
+    () => BibliographyTypeEntity,
+    (bibliographyType) => bibliographyType.books
+  )
+  @JoinColumn({ name: "bibliography_type_id", referencedColumnName: "id" })
+  bibliographyType: BibliographyTypeEntity;
 
-    @ManyToOne(() => ScienceEntity, (science) => science.books)
-    @JoinColumn({ name: 'science_id', referencedColumnName: 'id' })
-    science: ScienceEntity
+  @ManyToOne(() => PublisherEntity, (publisher) => publisher.books)
+  @JoinColumn({ name: "publisher_id", referencedColumnName: "id" })
+  publisher: PublisherEntity;
 
-    @OneToMany(() => RequestEntity, (request) => request.book)
-    requests: RequestEntity[]
+  @ManyToOne(() => LanguageEntity, (language) => language.books)
+  @JoinColumn({ name: "language_id", referencedColumnName: "id" })
+  language: LanguageEntity;
+
+  @ManyToOne(() => ScienceEntity, (science) => science.books)
+  @JoinColumn({ name: "science_id", referencedColumnName: "id" })
+  science: ScienceEntity;
+
+  @ManyToMany(() => GenreEntity, (genre) => genre.books)
+  @JoinTable({
+    name: "book-genres",
+    joinColumn: {
+      name: "bookId",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "genreId",
+      referencedColumnName: "id",
+    },
+  })
+  genres: GenreEntity[];
+
+  @OneToMany(() => RequestEntity, (request) => request.book)
+  requests: RequestEntity[];
 }
