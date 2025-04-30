c1a834ad54db05396c32bbbfa4a0b8a4f07c8b7b
TommyGC11 <tommycontreras34@gmail.com>
2025-04-30 16:53:09 -0400

    feat: :sparkles: refactor getAllCommentService to improve error handling and return structured comment tree

--- src/controllers/comment/getAll.controller.ts
+++ src/controllers/comment/getAll.controller.ts
@@ -1,54 +1,18 @@
 import { Request, Response } from "express";
+import { IsNull } from "typeorm";
 import { getAllCommentService } from "./../../services/comment/getAll.service";
 import { statusCode } from "./../../utils/status.util";
-import { ObjectStorage } from "./../../libs/object-storage";
 
 export const getAllCommentController = async (_req: Request, res: Response) => {
   getAllCommentService({
+    where: { parent: IsNull() },
     relations: {
       user: true,
-      book: true,
-      parent: true
-    }
+      book: true
+    },
   })
     .then(async (data) => {
-      const storage = ObjectStorage.instance;
-      const comments = await Promise.all(
-        data.map(async (comment) => {
-
-          return {
-            uuid: comment.uuid,
-            content: comment.content,
-            book: {
-              uuid: comment.book.uuid,
-              name: comment.book.name,
-            },
-            user: {
-              uuid: comment.user.uuid,
-              name: comment.user.name,
-            },
-            parentComment: {
-              uuid: comment?.parent?.uuid,
-              content: comment?.parent?.content,
-              user: {
-                uuid: comment?.parent?.user?.uuid,
-                name: comment?.parent?.user?.name,
-              },
-              book: {
-                uuid: comment?.parent?.book?.uuid,
-                name: comment?.parent?.book?.name,
-              },
-              ...(comment?.parent?.file_name && {
-                url: await storage.getUrl(comment?.parent?.file_name),
-              }),              
-            },
-            createdAt: comment.createdAt,
-            ...(comment?.file_name && { url: await storage.getUrl(comment?.file_name) })
-          };
-        })
-      );
-
-      return res.status(statusCode.OK).json({ data: comments });
+      return res.status(statusCode.OK).json({ data: data });
     })
     .catch((e) =>
       res
--- src/services/comment/getAll.service.ts
+++ src/services/comment/getAll.service.ts
@@ -1,20 +1,48 @@
-import { CommentEntity } from "./../../database/entities/entity/comment.entity";
 import { FindManyOptions } from "typeorm";
+import connection from "./../../database/connection";
+import { CommentEntity } from "./../../database/entities/entity/comment.entity";
 import { statusCode } from "./../../utils/status.util";
 
 export async function getAllCommentService(
   options?: FindManyOptions<CommentEntity>
 ) {
-  const comments = await CommentEntity.find(options).catch((e) => {
-    console.error("CommentEntity.find: ", e);
-    return null;
-  });
+  const comments = connection.getTreeRepository(CommentEntity);
 
-  if (!comments)
+  const topLevelComments = await comments.find(options);
+
+  if (!topLevelComments)
     return Promise.reject({
       message: "Comments not found",
       status: statusCode.NOT_FOUND,
     });
 
-  return comments;
+  const commentsTree = await Promise.all(
+    topLevelComments.map((comment) =>
+      comments.findDescendantsTree(comment, {
+        relations: ["user", "book"],
+      })
+    )
+  );
+
+  return commentsTree.map(formatComment);
 }
+
+export const formatComment = (comment: CommentEntity): any => {
+  return {
+    uuid: comment.uuid,
+    content: comment.content,
+    // ...(comment.file_name && { url: await storage.getUrl(comment.file_name) }),
+    ...(comment.file_name && { url: comment.file_name }),
+    status: comment.status,
+    createdAt: comment.createdAt,
+    user: {
+      uuid: comment.user.uuid,
+      name: comment.user.name,
+    },
+    book: {
+      uuid: comment.book.uuid,
+      name: comment.book.name,
+    },
+    replies: comment.replies?.map(formatComment) || [],
+  };
+};
